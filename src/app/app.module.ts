03243944673fdfbf860629760aa772ff5c35c1cd
dattebayorob <robson.william65@gmail.com>
2019-02-10 20:47:58 -0300

    Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    BodyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
