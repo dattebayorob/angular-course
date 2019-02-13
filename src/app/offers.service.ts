652e509e3048d7182e05e5dd0bfca768fcd1ba58
dattebayoRob <robson.william65@gmail.com>
2019-02-13 13:45:38 -0300

    Route lessons, return data dynamically from fake api

--- src/app/offers.service.ts
+++ src/app/offers.service.ts
@@ -1,6 +1,7 @@
 import { Offer } from './shared/offer.model'
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core';
+import { httpFactory } from '@angular/http/src/http_module';
 
 @Injectable()
 export class OffersService{
@@ -13,5 +14,9 @@ export class OffersService{
     public getOffers(): Promise<Array<Offer>> {
         return this.http.get('http://localhost:3000/offers')
             .toPromise().then((response: any)=> response.json());
+    }
+    public getOffersByCategory(category: string): Promise<Array<Offer>> {
+        return this.http.get('http://localhost:3000/offers?category='+category)
+            .toPromise().then((response:any) => response.json());
     }
 }
