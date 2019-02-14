c67e9945d98adb19e8911adffbc18230fa400fd7
dattebayoRob <robson.william65@gmail.com>
2019-02-14 10:04:45 -0300

    Return root params

--- src/app/offer/how/how.component.ts
+++ src/app/offer/how/how.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-how',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.parent.snapshot.params['id']);
   }
 
 }
